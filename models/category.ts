1d0506dec5c8487f89a9e2a1e62776fc3cbe6eb6
MonkeyAndEgg <412793874@qq.com>
2022-10-27 02:26:52 -0700

    added enums

---
+++ models/category.ts
@@ -0,0 +1,9 @@
+export enum CATEGORY {
+  FOOD = 'food',
+  UTILITY = 'utility',
+  ELECTRICITY = 'electricity',
+  INSURANCE = 'insurance',
+  MORTGAGE = 'mortgage',
+  GAME = 'game',
+  RENT = 'rent',
+}
---
+++ models/type.ts
@@ -0,0 +1,4 @@
+export enum BALANCE_ENTRY_TYPE {
+  INCOME = 'income',
+  COST = 'cost',
+}
